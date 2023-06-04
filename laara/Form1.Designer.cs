ce8161afcb36bb1c5318a004061ab9c7a39c6f24
Laarraa <112383573+Laarraa@users.noreply.github.com>
2023-06-04 16:44:46 +0200

    Add files via upload

---
+++ laara/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace CrudOperationUsingStoreProcedure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.showAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(182, 18);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(522, 35);
+            this.txtNumber.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Telefon:";
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnsave.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(483, 625);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(190, 48);
+            this.btnsave.TabIndex = 6;
+            this.btnsave.Text = "Ny";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.Btnsave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnUpdate.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(696, 625);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(192, 48);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Redigera";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnDelete.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(908, 625);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(195, 48);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Ta bort";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(71, 356);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1032, 263);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtFirstName.Location = new System.Drawing.Point(182, 88);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(522, 35);
+            this.txtFirstName.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 29);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Förnamn:";
+            // 
+            // txtClass
+            // 
+            this.txtClass.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtClass.Location = new System.Drawing.Point(182, 289);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(522, 35);
+            this.txtClass.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(66, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 29);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Klass:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtEmail.Location = new System.Drawing.Point(182, 222);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(522, 35);
+            this.txtEmail.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(66, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 29);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Email:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Arial", 12F);
+            this.txtLastName.Location = new System.Drawing.Point(182, 155);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(522, 35);
+            this.txtLastName.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(66, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 29);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Efternamn:";
+            // 
+            // showAll
+            // 
+            this.showAll.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.showAll.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showAll.Location = new System.Drawing.Point(249, 625);
+            this.showAll.Name = "showAll";
+            this.showAll.Size = new System.Drawing.Size(213, 48);
+            this.showAll.TabIndex = 21;
+            this.showAll.Text = "Visa lista";
+            this.showAll.UseVisualStyleBackColor = false;
+            this.showAll.Click += new System.EventHandler(this.ShowAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1284, 702);
+            this.Controls.Add(this.showAll);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Telefonbok";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button showAll;
+    }
+}
+
---
+++ laara/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudOperationUsingStoreProcedure
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbCommand cmd;
+        OleDbConnection conn;
+
+        public int ID { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Telefonbok.accdb");
+
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        //Student information från databas
+        private void StudentRecord()
+        {
+            conn.Open();
+            cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from information";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+
+        }
+
+        //Uppdaterings knapp 
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtNumber.Text != string.Empty && txtFirstName.Text != string.Empty && txtLastName.Text != string.Empty
+                && ValidEmail(txtEmail.Text) && txtClass.Text != string.Empty && ID != null)
+            {
+                cmd = conn.CreateCommand();
+                conn.Open();
+                cmd.CommandText = "update information set Förnamn='" + txtFirstName.Text + "',Efternamn='" + txtLastName.Text + "',Klass='" + txtClass.Text + "',Telefonnummer='" + txtNumber.Text + "',Email='" + txtEmail.Text + "' where id=" + ID + " ";
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Updated successfully.", "Record Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                conn.Close();
+                StudentRecord();
+                reset();
+            }
+            else
+            {
+                MessageBox.Show("Please enter value in all fields OR select row", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //Knapp som tar bort information
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete this record ? ", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            if (dialogResult == DialogResult.Yes && ID != null)
+            {
+                conn.Open();
+                cmd.CommandText = "delete from information where ID=" + ID +"";
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Deleted successfully.", "Record Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView1.DataSource = cmd.CommandText;
+                reset();
+                conn.Close();
+                StudentRecord();
+                btnDelete.Enabled = false;
+                btnUpdate.Enabled = false;
+                btnsave.Enabled = true;
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Please select row to delete the record", "Invalid Select", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //Knapp som sparar nya informationen
+        private void Btnsave_Click(object sender, EventArgs e)
+        {
+            if (txtNumber.Text != string.Empty  && txtFirstName.Text != string.Empty && txtLastName.Text != string.Empty
+                && ValidEmail(txtEmail.Text) && txtClass.Text != string.Empty)
+            {
+                cmd = conn.CreateCommand();
+                conn.Open();
+                cmd.CommandText = "insert into information (Förnamn,Efternamn,Klass,Telefonnummer,Email) values('" + txtFirstName.Text +"','"+ txtLastName.Text + "','"+ txtClass.Text + "','"+ txtNumber.Text + "','"+ txtEmail.Text + "')";
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+                reset();
+                conn.Close();
+                StudentRecord();
+            }
+            else
+            {
+                MessageBox.Show("Please enter value in all fields", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //startar om textfälten för namn, email, telefonnummer osv
+        private void reset()
+        {
+            txtFirstName.Text = "";
+            txtNumber.Text = "";
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtClass.Text = "";
+        }
+
+        private void ShowAll_Click(object sender, EventArgs e)
+        {
+            btnDelete.Enabled = false;
+            btnUpdate.Enabled = false;
+            StudentRecord();
+        }
+
+        //Data Grid View som visar hela telefonbok 
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                btnDelete.Enabled = true;
+                btnsave.Enabled = false;
+                btnUpdate.Enabled = true;
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                ID = Convert.ToInt32(row.Cells[0].Value);
+                txtFirstName.Text = row.Cells[1].Value.ToString();
+                txtLastName.Text = row.Cells[2].Value.ToString();
+                txtClass.Text = row.Cells[3].Value.ToString();
+                txtNumber.Text = row.Cells[4].Value.ToString();
+                txtEmail.Text = row.Cells[5].Value.ToString();
+            }
+        }
+
+        // Kollar så att email adressen är rätt annars kommer fel meddelande
+        bool ValidEmail(string email)
+        {
+            var trimmedEmail = email.Trim();
+
+            if (trimmedEmail.EndsWith("."))
+            {
+                MessageBox.Show("Email address in invalid", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                MessageBox.Show("Email address in invalid", "Invalid Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                btnDelete.Enabled = true;
+                btnsave.Enabled = false;
+                btnUpdate.Enabled = true;
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                ID = Convert.ToInt32(row.Cells[0].Value);
+                txtFirstName.Text = row.Cells[1].Value.ToString();
+                txtLastName.Text = row.Cells[2].Value.ToString();
+                txtClass.Text = row.Cells[3].Value.ToString();
+                txtNumber.Text = row.Cells[4].Value.ToString();
+                txtEmail.Text = row.Cells[5].Value.ToString();
+            }
+
+        }
+    }
+}
